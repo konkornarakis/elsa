a82f2d2b45bd01d014aaf6c3983221fe71a12991
sahgon <giorgos.korn@yahoo.gr>
2021-04-25 15:21:49 +0300

    first commit

---
+++ src/main/java/com/elsa/elsa/ElsaApplication.java
@@ -0,0 +1,13 @@
+package com.elsa.elsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elsa/elsa/ElsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsa.elsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
